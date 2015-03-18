9e1a0fdd43cb9a3ab7768ad3342bdffc9faa0d1f
jakoko <aerisgurlie777@gmail.com>
2015-03-18 01:02:36 -0700

    serve static assets

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app     = express();
+
+var mongoose  = require('mongoose');
+
+// Access to files in html
+app.use("/public", express.static(__dirname + '/public'));
+app.use("/node_modules", express.static(__dirname + '/node_modules'));
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + "/views/home.html")
+});
+
+app.listen(8080);
