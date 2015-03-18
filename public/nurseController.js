e0943b05fa9d74b0737a86bfb48f55f810fba791
jakoko <aerisgurlie777@gmail.com>
2015-03-18 09:55:25 -0700

    constants for dropdown options

--- public/nurseController.js
+++ public/nurseController.js
@@ -12,4 +12,30 @@ angular.module('nurseApp')
             { beginTime: 0930,
               endTime: 1100 }
         ];
+
+        // Time of day
+        $scope.timeOfDay = ["AM", "PM"];
+
+        // Hours Options
+        $scope.hours = [
+            { label: '1', value: 1 },
+            { label: '2', value: 2 },
+            { label: '3', value: 3 },
+            { label: '4', value: 4 },
+            { label: '5', value: 5 },
+            { label: '6', value: 6 },
+            { label: '7', value: 7 },
+            { label: '8', value: 8 },
+            { label: '9', value: 9 },
+            { label: '10', value: 10 },
+            { label: '11', value: 11},
+            { label: '12', value: 12 },
+        ];
+
+        $scope.minutes = [
+            { label: '00', value: 0 },
+            { label: '15', value: 15 },
+            { label: '30', value: 30 },
+            { label: '45', value: 45 },
+        ];
     };
